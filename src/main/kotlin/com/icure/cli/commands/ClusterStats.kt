582324d0a0c0db786f083e6d029786cb70c85e24
Vincenzo Pierro <vcp@icure.com>
2023-04-28 10:16:15 +0200

    Added cluster-stats and group-info commands

---
+++ src/main/kotlin/com/icure/cli/commands/ClusterStats.kt
@@ -0,0 +1,82 @@
+package com.icure.cli.commands
+
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.requireObject
+import com.github.ajalt.clikt.parameters.options.default
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.options.required
+import com.github.ajalt.clikt.parameters.options.switch
+import com.github.ajalt.clikt.parameters.types.path
+import com.icure.cli.CliktConfig
+import com.icure.cli.couchdb.DbStats
+import com.icure.cli.utils.getGroupHierarchy
+import io.ktor.client.call.*
+import io.ktor.client.request.*
+import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import java.io.FileWriter
+
+@ExperimentalSerializationApi
+class ClusterStats : CliktCommand(help = "Creates a report about the database in a server") {
+    private val config by requireObject<CliktConfig>()
+    private val format by option().switch(
+        "--json" to ExportFormat.JSON,
+        "--tsv" to ExportFormat.TSV
+    ).default(ExportFormat.JSON)
+    private val outputFile by option("-o", "--output").path().required()
+
+    private val json = Json {
+        prettyPrint = true
+        prettyPrintIndent = "  "
+    }
+
+    override fun run() {
+        runBlocking {
+            val dbs = config.client.get("${config.server}/_all_dbs").body<List<String>>()
+                .filter {
+                    Regex("icure-[a-zA-Z0-9\\-]+-(base|healthdata|patient)").matches(it)
+                }.map {
+                    config.client.get("${config.server}/$it").body<DbStats>()
+                }.groupBy {
+                    it.db_name.replace("icure-", "").replace(Regex("-(base|healthdata|patient)"), "")
+                }.map {
+                    it.key to DbGroupInfo(
+                        stats = it.value,
+                        groupHierarchy = config.getGroupHierarchy(it.key)
+                    )
+                }.toMap()
+            when(format) {
+                ExportFormat.JSON -> FileWriter(outputFile.toFile()).use { fw ->
+                    fw.write(json.encodeToString(dbs))
+                }
+                ExportFormat.TSV -> FileWriter(outputFile.toFile()).use { fw ->
+                    fw.write("GROUP_NAME\tROOT_SUPERGROUP\tBYTE_SIZE\n")
+                    dbs.forEach {
+                        fw.write(
+                            buildString {
+                                append("${it.key}\t")
+                                append("${it.value.groupHierarchy.first()}\t")
+                                val totalSize = it.value.stats.sumOf { it.sizes.file }
+                                append("${totalSize}\n")
+                            }
+                        )
+                    }
+                }
+            }
+
+        }
+    }
+
+    private enum class ExportFormat { JSON, TSV }
+
+    @Serializable
+    private data class DbGroupInfo(
+        val stats: List<DbStats>,
+        val groupHierarchy: List<String>
+    )
+
+}
+
---
+++ src/main/kotlin/com/icure/cli/commands/GroupInfo.kt
@@ -0,0 +1,61 @@
+package com.icure.cli.commands
+
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.requireObject
+import com.github.ajalt.clikt.parameters.options.flag
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.options.required
+import com.icure.cli.CliktConfig
+import com.icure.cli.couchdb.DbStats
+import com.icure.cli.utils.Size
+import com.icure.cli.utils.getGroupHierarchy
+import com.icure.cli.utils.humanReadableSize
+import io.ktor.client.call.*
+import io.ktor.client.request.*
+import kotlinx.coroutines.runBlocking
+
+class GroupInfo : CliktCommand(help = "Show the information about a group") {
+    private val config by requireObject<CliktConfig>()
+    private val group by option("-g", "--group", help = "The group to show the information for").required()
+    private val humanReadable by option("-H", help = "Show size values in Kb, Mb and Gb").flag()
+
+    override fun run() {
+        runBlocking {
+            val dbs = config.client.get("${config.server}/_all_dbs").body<List<String>>().filter {
+                it.matches(Regex("icure-$group-.+"))
+            }.map {
+                config.client.get("${config.server}/$it").body<DbStats>()
+            }
+            val hierarchy = config.getGroupHierarchy(group)
+            val output = if(dbs.isNotEmpty())
+                buildString {
+                    append("Databases for the group $group:\n")
+                    dbs.forEach { db ->
+                        append("\t${db.db_name}:\n")
+                        append("\t\tdocuments: ${db.doc_count}\n")
+                        if (humanReadable)
+                            append("\t\tsize: ${humanReadableSize(Size(db.sizes.file.toDouble())).let { "${it.value} ${it.unit.stringValue}" }}\n")
+                        else
+                            append("\t\tsize: ${db.sizes.file}\n")
+                        append("\n")
+                    }
+                    append("Total documents: ${dbs.sumOf { it.doc_count }}\n")
+                    val totalSize = dbs.sumOf { it.sizes.file.toDouble() }
+                    if (humanReadable)
+                        append("size: ${humanReadableSize(Size(totalSize)).let { "${it.value} ${it.unit.stringValue}" }}\n")
+                    else
+                        append("size: $totalSize b\n")
+                    append("Group hierarchy:\n")
+                    append("${hierarchy.first()}\n")
+                    hierarchy.drop(1).fold(0) { tabs, g ->
+                        append("${"\t".repeat(tabs)}|\n")
+                        append("${"\t".repeat(tabs)}*-$g\n")
+                        tabs + 1
+                    }
+                }
+                else "No databases found for group $group"
+            echo(output)
+        }
+    }
+
+}
