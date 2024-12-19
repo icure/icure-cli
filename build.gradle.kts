60b0ee70bd1f6c6647f9a01f90a037fbdff67892
aduchate <ad@icure.com>
2024-12-19 22:26:06 +0100

    Merge with VCP branch

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.9.21"
-    kotlin("plugin.serialization") version "1.9.21"
+    kotlin("jvm") version "2.0.20"
+    kotlin("plugin.serialization") version "2.0.20"
     id("com.github.johnrengelman.shadow") version "7.1.2"
 
     application
